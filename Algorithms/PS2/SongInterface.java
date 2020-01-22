8cbf1476179c0274e77469ca266383e9bacebcaf
sollisk <58785752+sollisk@users.noreply.github.com>
2020-01-22 16:54:48 -0600

    Add files via upload

---
+++ Algorithms/PS2/SongInterface.java
@@ -0,0 +1,8 @@
+
+public interface SongInterface{
+	public void sortBySong(String A[]);
+	public void insertionSort(int A[]);
+	public void bubbleSort(int A[]);
+	public void mergeSort(int A[]);
+
+}
