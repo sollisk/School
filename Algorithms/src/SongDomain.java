7d3c4d4a85556d067514444536bdf5658a8593c0
sollisk <58785752+sollisk@users.noreply.github.com>
2020-01-22 16:50:44 -0600

    Add files via upload

---
+++ Algorithms/src/SongDomain.java
@@ -0,0 +1,27 @@
+
+public class SongDomain {
+	String artist;
+	String songName;
+	int playTime;
+	
+	//Getters and Setters
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getSongName() {
+		return songName;
+	}
+	public void setSongName(String songName) {
+		this.songName = songName;
+	}
+	public int getPlayTime() {
+		return playTime;
+	}
+	public void setPlayTime(int playTime) {
+		this.playTime = playTime;
+	}
+
+}
---
+++ Algorithms/src/UAInterface.java
@@ -0,0 +1,12 @@
+
+public interface UAInterface{
+	public static void sortBySong() {
+	}
+	public void insertionSort(UASong A[]);
+	public void bubbleSort(UASong A[]);
+	public void mergeSort(UASong A[]);
+	
+	public static void loadArray() {
+
+}
+}
